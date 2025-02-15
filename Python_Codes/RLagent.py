516ad4ad4aab72107774c6b1f2de350dd13651ff
Amir Parvini <amirparvini009@gmail.com>
2025-02-15 20:14:55 +0330

    py format

---
+++ Python_Codes/RLagent.py
@@ -0,0 +1,238 @@
+import numpy as np
+# import cupy as cp
+import itertools
+
+
+class rlagent():
+    def __init__(self, alpha, gamma, epsilon, cRange, mRange, crossover, mutation):
+        
+        # the learning rate
+        self.alpha = alpha
+
+        # the discount rate
+        self.gamma = gamma
+
+        # the exploration rate
+        self.epsilon = epsilon
+                                   
+        self.actionSpace = list(itertools.product(cRange, mRange))
+        # get the rewards table
+        self.rewardSpace = np.array([200,   150,   100,   50,    25,
+                                     150,   113,   75,    38,    19,
+                                     100,   75,    50,    25,    13,
+                                     50,    38,    25,    113,   7,
+                                     0,     0,    -10,   -20,   -30,
+                                    -1000, -1500, -2000, -2500, -3000])
+       
+        # a dictionary of all possible states, where the state is the key, and the value is the index for the q table and rewards space
+        self.stateSpace = { '(VHC, VHD)': 0, '(VHC, HD)':1,  '(VHC, MD)':2,  '(VHC, LD)':3,  '(VHC, VLD)':4,
+                            '(HC, VHD)':5,   '(HC, HD)':6,   '(HC, MD)':7,   '(HC, LD)':8,   '(HC, VLD)':9,
+                            '(LC, VHD)':10,  '(LC, HD)':11,  '(LC, MD)':12,  '(LC, LD)':13,  '(LC, VLD)':14,
+                            '(VLC, VHD)':15, '(VLC, HD)':16, '(VLC, MD)':17, '(VLC, LD)':18, '(VLC, VLD)':19,
+                            '(S, VHD)':20,  '(S, HD)':21,  '(S, MD)':22,  '(S, LD)':23,  '(S, VLD)':24,
+                            '(I, VHD)':25,  '(I, HD)':26,  '(I, MD)':27,  '(I, LD)':28,  '(I, VLD)':29}
+
+        self.Q = np.zeros([len(self.stateSpace), len(self.actionSpace)])
+        
+        
+        # ----------------------------------------------- initialization  ------------------------------------------------
+        # a variable keeping track of how much rewards it has recieved
+        self.collected  = 0
+
+        # create an array to keep count how often each action was taken
+        self.actionCount = np.zeros(len(self.actionSpace))
+
+        # the previous fitness variable is initilized with a verh high cost
+        self.prevFitness = 10**10
+        
+        # the current fitness delta
+        self.fitness = 0
+
+        # the current diversity index
+        self.diversity = 1
+        
+        # the current reward awarded
+        self.reward = 0
+
+        # initialize the first state (high cost, and very high diversity)
+        self.currState = 0
+
+        # the first actions are given
+        self.action = self.actionSpace.index((crossover, mutation))
+    
+    
+    def __max(self, out, arr):
+        # hold any ties found
+        ties = []
+
+        # set an initial top value
+        top = float('-inf')
+
+        # for each element in the array
+        for i in range(len(arr)):
+
+            # if the current value is the new highest value
+            if arr[i] > top:
+
+                # then reset the tie list
+                ties = []
+
+                # set the new top value
+                top = arr[i]
+
+                # add the top value to the tie list
+                ties.append([i, arr[i]])
+
+            # else if the current value is tied to the highest value
+            elif arr[i] == top:
+
+                # then add it to the tie list
+                ties.append([arr[i], i])
+        
+        # pick a random index
+        choice = np.random.choice(np.arange(len(ties)))
+
+        # return the desired value
+        return ties[choice][out]
+    
+       
+    def __d_fitness(self, fitnesses):
+        # get the min fitness of the population
+        bestFitness = np.amin(fitnesses)
+        # obtaint the difference between the current and previous fitness values
+        delta = self.prevFitness - bestFitness
+        
+        # the difference is divided by the previous fitness to obtain a percentage
+        deltaFitness = delta / self.prevFitness
+        
+        # the current fitness is set as the previous fitness for the next iteration
+        self.prevFitness = bestFitness
+
+        # return the fitness imrpovement as a percenetage
+        return deltaFitness
+    
+    
+    def __diversity(self, population):
+        population = np.array(population)
+        sortarr = population[np.lexsort(population.T[::-1])]
+        mask = np.empty(population.shape[0], dtype=np.bool_)
+        mask[0] = True
+        mask[1:] = np.any(sortarr[1:] != sortarr[:-1], axis=1)
+        diversity = sortarr[mask].shape[0]/population.shape[0]
+        return diversity
+
+    def __reward(self):
+        # the reward is look up in the table
+        self.reward = self.rewardSpace[self.nextState]
+
+        # the rewards is added to the collection
+        self.collected += self.reward
+
+    # used for printing output
+    def __findState(self):
+        for i in self.stateSpace:
+            if self.stateSpace[i] == self.currState:
+                return i
+    
+    
+    def __state(self, df, di):
+        if df < 0:
+            fState = 'I'
+        elif df == 0:
+            fState = 'S'
+        elif df < 0.01:
+            fState = 'VLC'
+        elif df < 0.05:
+            fState = 'LC'
+        elif df < 0.25:
+            fState = 'HC'
+        else:
+            fState = 'VHC'
+
+        # an if statment to convert numerical values into into categorical bins
+        if di <= 0.2:
+            dState = 'VLD'
+        elif di <= 0.4:
+            dState = 'LD'
+        elif di <= 0.6:
+            dState = 'MD'
+        elif di <= 0.8:
+            dState = 'HD'
+        else:
+            dState = 'VHD'
+            
+        state = '(' + fState + ', ' + dState + ')'
+        self.nextState = self.stateSpace[state]
+        self.reward = self.rewardSpace[self.nextState]
+        
+        
+    def initAction(self):
+        # reset the action count to disregard the first action
+        self.actionCount = np.zeros(len(self.actionSpace))
+
+        # the action count is updated
+        self.actionCount[self.action] += 1
+        
+        # update the results log
+        # self.__results(0)
+
+        # give the enviroment its action (the crossover and mutation probability)
+        return self.actionSpace[self.action][0], self.actionSpace[self.action][1]
+
+    def decide(self):
+        # randomly decide to explore (with probability epsilon)
+        if np.random.random() <= self.epsilon:
+
+            # a random action is chosen
+            self.action = int(np.random.randint(low=0, high=len(self.actionSpace)))
+
+        # or exploit (with probability 1 - epsilon)
+        else:
+
+            # the max action is chosen
+            self.action = int(self.__max(0, (self.Q[self.currState])))
+        
+        # the action count is updated
+        self.actionCount[self.action] += 1
+
+        # print and save the results
+        # self.__results(count)
+
+        # give the enviroment its action (the crossover and mutation probability)
+        return self.actionSpace[self.action][0], self.actionSpace[self.action][1]
+        
+        
+    # the agent observes the enviroment's response to the agent's action
+    def observe(self, population, fitnesses):
+        # obtain the population and their fitnesses after an action
+        
+        # determine the delta of the previous fitness and the current best fitness of the population and the diversity 
+        self.fitness = self.__d_fitness(fitnesses)
+        self.diversity = self.__diversity(population)
+        
+        # get the new state and rewards
+        self.__state(self.fitness, self.diversity)
+        self.__reward()
+
+    # the Q table is updated along with other variables for the q learning algorithm
+    def updateQlearning(self):
+        # update the q table using the bellman equation
+        self.Q[self.currState, self.action] += self.alpha * (self.reward + self.gamma * self.__max(1, self.Q[self.nextState]) - self.Q[self.currState, self.action] )
+
+        # update the current state
+        self.currState = self.nextState
+
+        
+        
+        
+# cRange = np.array(range(1, 10))/10
+# mRange = np.array(range(1, 10))/10
+# alpha = 0.7
+# gamma = 0.1
+# epsilon = 0.3
+# crossover = 0.8
+# mutation = 0.2
+# agent = rlagent(alpha, gamma, epsilon, cRange, mRange, crossover, mutation)
+
+# agent = rlagent()
+# print(agent.actionSpace[0])
---
+++ Python_Codes/genetic.py
@@ -0,0 +1,418 @@
+from RLagent import rlagent
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import matplotlib.markers as mks
+import numpy as np
+import pprint
+import random as rn
+from itertools import chain
+import math
+import pandas as pd
+
+
+# Parameters of Reinforcement Learning
+cRange = np.array(range(1, 10))/10
+mRange = np.array(range(1, 10))/10
+alpha = 0.7
+gamma = 0.1
+epsilon = 0.3
+c = 0.8
+m = 0.2
+agent = rlagent(alpha, gamma, epsilon, cRange, mRange, c, m)
+
+
+
+# Parameters for standard configuration
+NT = 400  # total number of TDR facilities available
+ST = 0.975 # safety level threshold
+M = 10**6  # total demands threshold
+L = 1000  # Lambda: scaling factor for total number of facilities opened
+A = 10  # Alpha: maximum number of neighborhoods that a neighborhood can serve
+B = 10  # Beta: maximum number of neighborhoods that a neighborhood can be served by
+G = 15  # Gamma: penalty for unit unmet demand
+k = ["Water", "Food", "Medical Kit"]  # commodities type
+v = dict(zip(k, [1, 1, 0.5]))  # unit volume of commodity k
+w = dict(zip(k, [1, 0.25, 0.25]))  # unit weight of commodity k
+cd = dict(zip(k, [2.5, 3, 0.333]))  # commodities demand of an injured in 1day
+V = 34560*5 # Volume Capacity of TDR
+W = 30480 # Weight Capacity of TDR
+
+
+# Reading the File
+n = [] # maximum number of TDR facilities that can be allocated in neighborhood i
+s = [] # safety level of neighborhood i
+elite_Chromosoms = []
+elites_fitness = math.inf
+x_nodes = []
+y_nodes = []
+No_nodes = 0
+No_supplierNodes = 0
+No_demandNodes = 0
+supplier_nodes_index = []
+demand_nodes_index = []
+v_capacity = []
+w_capacity = []
+v_demands = []
+w_demands = []
+dist = [] # distance between neighborhoods i and j
+def readfile(path: str):
+    global No_nodes, No_supplierNodes, No_demandNodes, supplier_nodes_index, demand_nodes_index, n, v_demands
+    with open(path, "r", encoding='utf-8') as file:
+        lines = file.readlines()
+    # print(lines)
+    for i, line in enumerate(lines, start=1):
+        if 2 < i:
+            NodesAttributes = line.split(",")
+            x_nodes.append(float(NodesAttributes[7]))
+            y_nodes.append(float(NodesAttributes[8]))
+            population = int(NodesAttributes[4])
+            No_facility = int(NodesAttributes[6])
+            n.append(No_facility)
+            s.append(float(NodesAttributes[5]))
+            v_capacity.append(V*(No_facility))
+            w_capacity.append(W*No_facility)
+            v_demands.append(sum([x * y * population for x, y in zip(list(v.values()), list(cd.values()))]))
+            w_demands.append(sum([x * y * population for x, y in zip(list(w.values()), list(cd.values()))]))
+    print("x_nodes = ", x_nodes, "\ny_nodes = ", y_nodes, "\nv_capacity = ",
+        v_capacity, "\nw_capacity = ", w_capacity, "\nv_demands = ",
+        v_demands, "\nw_demands =", w_demands, "\nSafty Levels = ", s)
+    
+    print("\nSumVolumeCapacities = ", sum(v_capacity), "\nSumWeightCapacities = ", sum(w_capacity),
+          "\nSumVolumeDemands = ", sum(v_demands), "\nSumWeightDemands = ", sum(w_demands))
+
+    No_nodes = len(x_nodes)
+    supplier_nodes_index = [index for index, s in enumerate(s) if s >= ST]
+    demand_nodes_index = [index for index, s in enumerate(s) if s < ST]
+    No_supplierNodes = len(supplier_nodes_index)
+    No_demandNodes = len(demand_nodes_index)
+    print("\nNum of Nodes = ",No_nodes,
+          "\nNum of Spplier Nodes = ", No_supplierNodes,
+          "\nNum of demand Nodes = ", No_demandNodes,
+          "\nSupplier Nodes Index = ", supplier_nodes_index,
+          "\nDemand Nodes Index = ", demand_nodes_index)
+    
+
+readfile("C:\\Users\\Amir\\Desktop\\SDSS-Project\\Data\\Neighborhood_Data.txt")    
+
+
+
+
+
+def DistMatrix():
+    distmatrix = np.empty((No_nodes, No_nodes), dtype=float)
+    for i in range(No_nodes):
+        for j in range(No_nodes):
+            distmatrix[i][j] = round(math.sqrt( pow(x_nodes[i]-x_nodes[j],2) + pow(y_nodes[i]-y_nodes[j],2)), 2)
+    return distmatrix
+distmatrix = DistMatrix()
+pprint.pprint(distmatrix)
+
+
+
+# Initial Population
+Population = []
+def generate_initial_population(pop_size):
+    for ـ in range(pop_size):
+        Chromosom = []
+        selectable = demand_nodes_index.copy()
+        for i in range(No_nodes):
+            if (i <= len(demand_nodes_index)-1):
+                j = rn.choice(selectable)
+                selectable.remove(j)
+            else:
+                j = rn.choice(range(min(n),n[supplier_nodes_index[i-len(demand_nodes_index)]]+1))
+            Chromosom.append(j)
+
+        Population.append(Chromosom)
+    return Population
+Population = generate_initial_population(300)
+# print(Population)
+
+
+
+
+
+# Fitness
+def distance(pop: list):
+    distance_fitness = []
+    d_supp = np.array([v_demands[s] for s in supplier_nodes_index])
+    c_supp = np.array([i*V  for i in pop[No_demandNodes:]])- d_supp
+    d = np.array([v_demands[i] for i in demand_nodes_index])
+    j = 0
+    end = False
+    for i in range(No_supplierNodes):
+        while c_supp[i] >= d[j]:
+            distance_fitness.append(distmatrix[supplier_nodes_index[i], pop[j]])
+            c_supp[i] -= d[j]
+            d[j] = 0
+            j += 1
+            if j == No_demandNodes:
+                end = True
+                break
+        if end == True:
+            break
+        d[j] -= c_supp[i]
+        c_supp[i] = 0
+    return sum(distance_fitness)
+def fitness(population: list):
+    global elite_Chromosoms, elites_fitness
+    Fitness = []
+    for pop in population:
+        
+        sum_facilities_fitness = sum(pop[No_demandNodes: len(pop)])
+        
+        d = sum(v_demands)
+        c = sum([V*i for i in pop[No_demandNodes:]])
+        if d-c > 0:
+            unmet_demand_fitness = (d-c)
+        else:
+            unmet_demand_fitness = 0
+        
+        distance_fitness = distance(pop)
+
+            
+        Fitness.append(sum_facilities_fitness*L + unmet_demand_fitness*G + distance_fitness)
+    if min(Fitness) < elites_fitness:
+        elites_fitness = min(Fitness)
+        elite_Chromosoms = Population[Fitness.index(min(Fitness))]
+    return Fitness
+# Fitness = fitness(Population)
+# print(Fitness)
+
+
+
+# SRS Selection
+Ranking = range(1,len(Population)+1)
+K = len(Ranking)
+ChromosomsProb =[]
+ProbsRange = []
+for i in Ranking:
+    if i <= K/2:
+        ChromosomsProb.append((12*i)/(5*K*(K+2)))
+    else:
+        ChromosomsProb.append((28*i)/(5*K*(3*K+2)))
+for index, i in enumerate(ChromosomsProb):
+    if index == 0:
+        ProbsRange.append([0,i])
+    else:
+        ProbsRange.append([ProbsRange[index-1][1], ProbsRange[index-1][1] + (i)])
+def SRS_Selection(ChromosomsFitness: list):
+    selectedchromosomforcrossover = []
+    SelectedChromosomForCrossOver = []
+    if elites_fitness not in ChromosomsFitness: 
+        Population[rn.randint(0,len(Population)-1)] = elite_Chromosoms
+    ChromosomsFitnessSorted = sorted(ChromosomsFitness, reverse=True)
+    ChromosomsIndexByFitness = [ChromosomsFitness.index(i) for i in ChromosomsFitnessSorted]
+    for _ in Ranking:
+        r = rn.uniform(0,1)
+        for index, j in enumerate(ProbsRange):
+            if r > j[0] and r <= j[1]:
+                selectedchromosomforcrossover.append(ChromosomsIndexByFitness[index])
+                if len(selectedchromosomforcrossover) == 2:
+                    SelectedChromosomForCrossOver.append(selectedchromosomforcrossover)
+                    selectedchromosomforcrossover = []
+                    break
+    # print(f"SelectedChromosomForCrossOver length = {len(SelectedChromosomForCrossOver)}")
+    return SelectedChromosomForCrossOver
+# selectedChromosom = SRS_Selection(Fitness)
+# print(selectedChromosom)
+
+
+
+# OX Crossover
+def PMX_Crossover(selectedcrossover, crossoverprob):
+    childs = []
+    for i in selectedcrossover:
+        r = rn.uniform(0,1)
+        if r <= crossoverprob:
+            p1 = Population[i[0]]
+            p2 = Population[i[1]]
+            child1 = [1]*len(p1)
+            child2 = [1]*len(p1)
+            crossoverindex = rn.sample(list(range(len(demand_nodes_index))),2)
+            bp1 = p1[min(crossoverindex):max(crossoverindex)+1]
+            bp2 = p2[min(crossoverindex):max(crossoverindex)+1]
+            child1[min(crossoverindex):max(crossoverindex)+1] = bp2.copy()
+            child2[min(crossoverindex):max(crossoverindex)+1] = bp1.copy()
+            Chain = chain(range(min(crossoverindex)), range(max(crossoverindex)+1, len(child1)))
+            for j in Chain:
+                chekpoint1 = p1[j]
+                chekpoint2 = p2[j]
+                m = 0
+                ch = False
+                while chekpoint1 in bp2:
+                    m += 1
+                    chekpoint1 = bp1[bp2.index(chekpoint1)]
+                    if m > len(bp2):
+                        ch = True
+                        child1[j] = p2[j]
+                        break
+                if ch == False:
+                    child1[j] = chekpoint1
+                m = 0
+                ch = False
+                while chekpoint2 in bp1:
+                    m += 1
+                    chekpoint2 = bp2[bp1.index(chekpoint2)]
+                    if m > len(bp1):
+                        ch = True
+                        child2[j] = p1[j]
+                        break
+                if ch == False:
+                    child2[j] = chekpoint2
+                
+                
+            for index, i in enumerate(p2[len(demand_nodes_index):]):
+                child1[len(demand_nodes_index)+index] = i
+                child2[len(demand_nodes_index)+index] = p1[len(demand_nodes_index)+index]
+                
+            childs.append(child1)
+            childs.append(child2)
+            
+        else:
+            childs.append(Population[i[0]])
+            childs.append(Population[i[1]])
+    return childs
+
+
+
+
+# Mutation
+def Mutation(childs: list, mutationprob):
+    childsaftermutation = []
+    for i in childs:
+        r = rn.uniform(0,1)
+        if r <= mutationprob:
+            rnd = rn.sample(list(range(len(demand_nodes_index))),2)
+            rnd0 = i[rnd[0]]
+            rnd1 = i[rnd[1]]
+            i[rnd[0]] = rnd1
+            i[rnd[1]] = rnd0
+            childsaftermutation.append(i)
+            
+            # rnd = rn.sample(list(range(len(demand_nodes_index), len(i))),2)
+            # rnd0 = i[rnd[0]]
+            # rnd1 = i[rnd[1]]
+            # if n[rnd[1]] > rnd0 and n[rnd[0]] > rnd1:
+            #     i[rnd[0]] = rnd1
+            #     i[rnd[1]] = rnd0
+            # childsaftermutation.append(i)
+        else:
+            childsaftermutation.append(i)
+    return childsaftermutation
+# mutation = Mutation(childs)
+# print(mutation)
+
+
+
+# Generation
+def Generation(repeat):
+    global Population
+    g = range(1,repeat+1)
+    minfit = []
+    for i in range(repeat):
+        
+        if i == 0:
+            c, m = agent.initAction()
+        else:
+            c, m = agent.decide()
+            # print("Crossover Prob = ", c, "Mutation prob = " ,m)
+        
+        Fitness = fitness(Population)
+        agent.observe(Population, Fitness)
+        agent.updateQlearning()
+        minfit.append(min(Fitness) if (min(Fitness) <= elites_fitness) else elites_fitness)
+        SelectedChromosoms = SRS_Selection(Fitness)
+        Childs = PMX_Crossover(SelectedChromosoms, c)
+        childsaftermutation = Mutation(Childs, m)
+        Population = childsaftermutation
+    # Chromosom_Plot(InitialChromosoms[Fitness(InitialChromosoms).index(min(Fitness(InitialChromosoms)))])
+    best_solution = Population[fitness(Population).index(min(fitness(Population)))]
+    
+    df = pd.DataFrame(agent.Q)
+    # تغییر تنظیمات برای نمایش همه سطرها و ستون‌ها
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    # نمایش DataFrame
+    print(df)
+    
+    plt.plot(g,minfit)
+    plt.title(f'min_fitness = {minfit[-1]}')
+    # plt.figure(1)
+    plt.show()
+    return best_solution
+best_solution = Generation(2000)
+print("Best Solution = ",best_solution)
+print("\nSumDemand = ", sum(v_demands), "\nSumSupply = ", sum(V*n for n in best_solution[No_demandNodes:]))
+print("\nTotal Facilities opened = ",sum(best_solution[No_demandNodes:]),
+      "\nUnmet Demand = ", sum(v_demands)-sum(V*n for n in best_solution[No_demandNodes:]),
+      "\nTotal Distance = ",distance(best_solution))
+
+
+
+def chromosomplot(bestsolution):
+    
+# display shp file
+    shapefile_path = "C:\\Users\\Amir\\Desktop\\SDSS-Project\\Data\\Mahallat\\Reg1_3_4.shp"
+    polygons = gpd.read_file(shapefile_path)
+    # print(polygons.head())
+    polygons.plot(edgecolor='black', facecolor='lightblue', figsize=(10, 8))
+    plt.title("Polygon Map")
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+
+# display nodes
+    x_d = []
+    y_d = []
+    x_s = []
+    y_s = []
+    for i in demand_nodes_index:
+        x_d.append(x_nodes[i])
+        y_d.append(y_nodes[i])
+    plt.scatter(x_d, y_d, s=5)
+    for i in supplier_nodes_index:
+        x_s.append(x_nodes[i])
+        y_s.append(y_nodes[i])
+    plt.scatter(x_s, y_s, c='r', s=15, marker='s')
+    
+    for n, txt in enumerate(range(No_nodes)):
+        plt.text(x_nodes[n], y_nodes[n], n, fontsize=6, ha='right', va='bottom')
+    
+# display allocating nodes
+    d_supp = np.array([v_demands[s] for s in supplier_nodes_index])
+    c_supp = np.array([i*V  for i in best_solution[No_demandNodes:]]) - d_supp
+    d = np.array([v_demands[i] for i in demand_nodes_index])
+    j = 0
+    end = False
+    for i in range(No_supplierNodes):
+        x = []
+        y = []
+        # print(f"c_supp = {c_supp} \n d = {d} \n")
+        while c_supp[i] >= d[j]:
+            c_supp[i] -= d[j]
+            d[j] = 0
+            # print(f"c_supp = {c_supp} \n d = {d} \n")
+            x.append(x_nodes[supplier_nodes_index[i]])
+            y.append(y_nodes[supplier_nodes_index[i]])
+            x.append(x_nodes[bestsolution[j]])
+            y.append(y_nodes[bestsolution[j]])
+            plt.plot(x,y)
+            x= []
+            y = []
+            j += 1
+            if j == No_demandNodes:
+                end = True
+                break
+        if end == True:
+            break
+        d[j] -= c_supp[i]
+        c_supp[i] = 0
+        # print(f"c_supp = {c_supp} \n d = {d} \n")
+        x.append(x_nodes[supplier_nodes_index[i]])
+        y.append(y_nodes[supplier_nodes_index[i]])
+        x.append(x_nodes[bestsolution[j]])
+        y.append(y_nodes[bestsolution[j]])
+        plt.plot(x,y)
+    plt.figure(1)
+    plt.show()
+chromosomplot(best_solution)    
