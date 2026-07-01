cfacd593ceea5f5457a3609ad06b966c9fd78580
Amir Parvini <amirparvini009@gmail.com>
2026-07-01 11:33:53 +0330

    backend: added geometry to Node model casts

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -9,6 +9,7 @@ class Node extends Model
     protected $fillable = ['type', 'name', 'geometry'];
 
     protected $casts = [
+        'geometry' => 'array',
         'area' => 'float',
         'capacity' => 'int',
         'affected_pop' => 'int'
