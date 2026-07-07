b3ff5fe57d9a0b7214c4ff46e71e09b4245a0c8e
Amir Parvini <amirparvini009@gmail.com>
2026-07-07 18:09:18 +0330

    backend: scenario service for scenario selection event created

--- app/Services/ScenarioService.php
+++ app/Services/ScenarioService.php
@@ -6,6 +6,16 @@ use App\Models\Scenario;
  
 class ScenarioService
 {
+    static function selectScenario(int $current_scenario_id){
+
+        // First, set all scenarios to inactive
+        Scenario::query()->update(['active' => 0]);
+        
+        // Then, set the current scenario to active
+        Scenario::where('id', $current_scenario_id)->update(['active' => 1]);
+
+    }
+
     public function getActiveScenarioId(): int
     {
         return Scenario::where('active', 1)->value('id');
